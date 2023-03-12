7ae8c5935a929b2cd89160791d9477dadef8f3d8
Hung Bui <hungbui7690@gmail.com>
2023-03-12 10:56:55 -0600

    Introducing RTL Book

---
+++ rtl-book/app.js
@@ -0,0 +1,10 @@
+/*
+  Introducing RTL Book
+  > pic
+  > npx rtl-book serve roles-notes.js
+    - create file roles-notes.js and saves notes in there
+
+  >  http://localhost:4005 to see the notes
+
+  (1) add elements
+*/
